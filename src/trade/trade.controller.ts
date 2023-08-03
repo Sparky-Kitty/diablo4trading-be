8e36fb1607900fc88e6ce88ead1e724eaaa1958a
Kyusung4698 <38520678+Kyusung4698@users.noreply.github.com>
2023-08-03 12:36:19 +0200

    - updated trade controller with new types

--- src/trade/trade.controller.ts
+++ src/trade/trade.controller.ts
@@ -1,64 +1,107 @@
 // trade.controller.ts
-import { Controller, Get, OnModuleInit, Query } from '@nestjs/common';
-import { generateMockDiabloItems } from '../diabloItems/diablo-item.mock';
-import { API } from '@sanctuaryteam/shared';
 import { Game } from '@diablosnaps/common';
+import {
+  Body,
+  Controller,
+  Get,
+  OnModuleInit,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { API } from '@sanctuaryteam/shared';
+import { SearchResult } from '@sanctuaryteam/shared/cjs/api/search';
 import { DiabloItem } from 'src/diabloItems/diablo-item.interface';
 import { DiabloItemService } from 'src/diabloItems/diablo-item.service';
-
-export interface SearchRequest {
-  payload: API.SearchPayload;
-  serverType: Game.ServerType;
-}
-
-export interface SearchPageRequest extends SearchRequest {
-  timestamp: number;
-  page: number;
-}
-
-export interface SearchPageResponse {
-  results: DiabloItem[];
-}
-
-export interface SearchResponse extends SearchPageResponse {
-  total: number;
-  timestamp: number;
-}
+import { generateMockDiabloItems } from '../diabloItems/diablo-item.mock';
 
 @Controller('trade')
 export class TradeController implements OnModuleInit {
   private diabloItemsMock: DiabloItem[] = [];
 
-  constructor(
-    private diabloItemService: DiabloItemService,
-  ) {
-  }
+  constructor(private diabloItemService: DiabloItemService) { }
 
   async onModuleInit() {
     const diabloItemAffixes = await this.diabloItemService.getAffixes();
     this.diabloItemsMock = generateMockDiabloItems(500, diabloItemAffixes);
   }
 
+  @Post('search')
+  createSearch(@Body() request: API.SearchPostRequest): API.SearchPostResponse {
+    console.info('createSearch', request.payload);
+    // TODO: store payload in db and return id
+    // same payload should return same id -> hash?
+    return {
+      payloadId: '1',
+      timestamp: Date.now(),
+    };
+  }
+
   @Get('search')
-  search(@Query() request: SearchPageRequest): SearchResponse {
-    const { payload, serverType, timestamp, page } = request;
+  getSearch(
+    @Query() request: API.SearchGetRequestParams,
+  ): API.SearchGetResponse {
+    console.info('getSearch', request);
+    // TODO: fetch payload from db and return it
+    return {
+      payload: {},
+    };
+  }
+
+  @Get('fetch')
+  fetch(@Query() request: API.FetchGetRequestParams): API.FetchGetResponse {
+    const { serverType, payloadId, timestamp, page } = request;
 
     // You can implement the actual search logic here based on the request
     // For now, we'll use mock data
     const startIndex = (page ?? 1 - 1) * 10;
     const endIndex = startIndex + 10;
-    console.log(startIndex, endIndex)
-    const paginatedResults: DiabloItem[] = this.diabloItemsMock.slice(startIndex, endIndex);
+    console.log(startIndex, endIndex);
+    const paginatedResults: DiabloItem[] = this.diabloItemsMock.slice(
+      startIndex,
+      endIndex,
+    );
 
-    const response: SearchPageResponse = {
-      results: paginatedResults,
+    const response: API.FetchGetResponse = {
+      results: paginatedResults.map<SearchResult>((item) => ({
+        item: {
+          // should be removed from item interface
+          language: undefined,
+          quality: item.quality,
+          variant: item.variant,
+          type: item.type,
+          power: item.power,
+          requiredLevel: item.requiredLevel,
+          classRestriction: item.classRestriction,
+          inherentAffixes: [
+            { affix: item.inherentAffix0, value: item.inherentAffix0Value },
+            { affix: item.inherentAffix1, value: item.inherentAffix1Value },
+          ]
+            .filter((x) => !isNaN(x.affix?.id))
+            .map<Game.ItemAffix>(({ affix, value }) => ({
+              id: `${affix.id}`,
+              value,
+            })),
+          affixes: [
+            { affix: item.affix0, value: item.affix0Value },
+            { affix: item.affix1, value: item.affix1Value },
+            { affix: item.affix2, value: item.affix2Value },
+            { affix: item.affix3, value: item.affix3Value },
+          ]
+            .filter((x) => !isNaN(x.affix?.id))
+            .map<Game.ItemAffix>(({ affix, value }) => ({
+              id: `${affix.id}`,
+              value,
+            })),
+          socketType: item.socketType,
+        },
+        listing: {
+          account: null,
+          expiresAt: null,
+          id: item.uuid,
+        },
+      })),
+      hasMore: endIndex < this.diabloItemsMock.length,
     };
-
-    const total = this.diabloItemsMock.length;
-    return {
-      total,
-      timestamp,
-      ...response,
-    };
+    return response;
   }
 }
